af3f78e5e280eb540c12906edfe3e54b235d8686
ursky <urskyx@gmail.com>
2017-12-28 14:17:38 -0500

    remade most figures

--- bin_classification_figures/bin_classification_pie_chart/parse.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-for line in open(sys.argv[1]):
-	t=line.strip().split("\t")[1].split(";")
-	for i in range(len(t)): t[i]="_".join(t[i].split("_")[:-1])
-	print "\t".join(t)
-	
---
+++ classify_bins_figures/bin_classification_pie_chart/parse2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import sys
+for line in open(sys.argv[1]):
+	print "1\t"+line.strip()
+	
